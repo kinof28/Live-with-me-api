c341b517b893941f9c63f86609124c194061391c
@kinof28 <ab28fb@gmail.com>
2021-03-19 03:28:41 +0100

    first commit

---
+++ src/main/java/com/Abdelwahab/Live/With/ME/LiveWithMeApplication.java
@@ -0,0 +1,13 @@
+package com.Abdelwahab.Live.With.ME;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveWithMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveWithMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Abdelwahab/Live/With/ME/LiveWithMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Abdelwahab.Live.With.ME;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveWithMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
